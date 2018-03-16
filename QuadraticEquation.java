a5bba9a8b6fb5bf8f3dc178d9a7a0bea08e1b7f6
nguyenkhacthe94 <nguyenkhacthe94@gmail.com>
2018-03-16 16:57:21 +0700

    Quadratic equation resolver

---
+++ QuadraticEquation.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class QuadraticEquation {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to Quadratic Equation Resolver");
+        System.out.println("Give a question as: 'a*x*x + b*x + c = 0', please enter your constant:");
+        System.out.println("a= ");
+        double a = input.nextDouble();
+        System.out.println("b= ");
+        double b = input.nextDouble();
+        System.out.println("c= ");
+        double c = input.nextDouble();
+
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) System.out.println("Equation has countless solutions.");
+                else System.out.println("Equation has no solution.");
+            } else {
+                double x = -c / b;
+                System.out.println("Equation has only one solution: x= " + x);
+            }
+        } else {
+            double delta = b * b - 4 * a * c;
+            if (delta < 0) System.out.println("Equation has no solution");
+            else {
+                double x1 = (-b - Math.sqrt(delta)) / (2 * a);
+                double x2 = (-b + Math.sqrt(delta)) / (2 * a);
+                System.out.println("Equation has 2 solutions, they are: " + x1 + " and " + x2);
+            }
+
+        }
+    }
+}
